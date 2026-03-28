7ac9a93096d869cf04b8eca0c8186aec9c9de02c
nimtechnologiez <s.saikumar2780@gmail.com>
2026-03-28 13:53:52 +0530

    Fix login 500 error and CSRF issue

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,7 +118,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 🌐 CORS
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://brolichi.com",
+    "https://www.brolichi.com"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://brolichi.com",
+    "https://www.brolichi.com"
+]
 
 # 🔒 Security (Production only)
 if os.environ.get('PRODUCTION') == 'True':
